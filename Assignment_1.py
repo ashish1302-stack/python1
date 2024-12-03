729657ed8fadaa4f710bc6f789466da3f6006290
ashish1302-stack <ashishmjose36@gmail.com>
2024-12-03 20:58:11 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,73 @@
+
+# print("Ashish")
+# print("D41")
+# print("ashish@example.com")
+
+# print("Ashish\nD41\nashish@example.com")
+
+# Exercise 3
+# num1 = 14
+# num2 = 7
+# print(num1, "+", num2, "=", num1 + num2)
+# print(num1, "-", num2, "=", num1 - num2)
+# print(num1, "*", num2, "=", num1 * num2)
+# print(num1, "/", num2, "=", num1 / num2)
+
+# Exercise 4
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# Exercise 5
+# print("\"SDK\" stands for \"Software Development Kit\","
+#       "\nwhereas \"IDE\" stands for \"Integrated "
+#       "Development Environment\".")
+
+
+# Exercise 6
+# print("python is an \"awesome\" language.")
+# print("python\n\t2023")
+# print('I\'m from Entri.\b')
+# print("\65")
+# print("\x65")
+# print("Entri", "2023", sep="\n")
+# print("Entri", "2023", sep="\b")
+# print("Entri", "2023", sep="*", end="\b\b\b\b")
+
+# Exercise 7
+num = 23
+textnum = "57"
+decimal = 98.3
+
+# print("Type of num:", type(num))
+# print("Type of textnum:", type(textnum))
+# print("Type of decimal:", type(decimal))
+
+# Convert
+# total = num + int(textnum) + decimal
+# print("Sum of variables:", total)
+# print("Type of the sum:", type(total))
+
+# # Exercise 8
+# days_in_year = 365
+# hours_in_day = 24
+# minutes_in_hour = 60
+#
+# # Calculate total minutes in a year
+# total_minutes = days_in_year * hours_in_day * minutes_in_hour
+# print("This program calculates the number of minutes in a year.")
+# print("The total number of minutes in a year is:", total_minutes)
+
+# # Exercise 9
+# name = input("Please enter your name: ")
+# print("Hi", name + ", welcome to Python programming :)")
+
+
+# Exercise 10
+pounds = float(input("Please enter the amount in pounds: "))
+conversion_rate = 1.35
+dollars = pounds * conversion_rate
+print(f"£{pounds} equals ${dollars:.2f}")
+
